ca9a67d161f1dabed91e5844ac169f60db0a7c59
Taweechai Yuenyang <krumii.it@gmail.com>
2023-05-19 16:34:44 +0700

    test buy crypto

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ fiatList = ["AUD", "BRL", "EUR", "GBP", "RUB",
             "TRY", "UAH", "ZAR", "IDRT", "PLN", "RON", "ARS"]
 
 crytoList = ['BTC', 'ETH', 'ADA', 'USDT', 'XRP', 'LINK', 'MANA', 'XLM', 'BNB', 'DOGE', 'TRX', 'ALGO', 'BUSD', 'ENJ', 'DOT', 'NEAR',
-             'SAND', 'GRT', 'MATIC', 'AXS', 'IMX', 'GALA', 'CHZ', 'SOL', 'AVAX', 'FTM', 'APE', 'GAL', 'ATOM', 'ARB', 'ID']
-advCash = ["RUB", "EUR"]
+             'SAND', 'GRT', 'MATIC', 'AXS', 'GALA', 'CHZ', 'SOL', 'AVAX', 'FTM', 'APE', 'GAL', 'ATOM', 'ARB', 'ID']
+advCash = ["RUB", "EUR", "GBP"]
 
 # Replace the placeholders with your actual database connection details
 host = 'localhost'
@@ -48,18 +48,17 @@ def main():
 
                 cursor.execute(sql)
 
-            # For bitkub
-            response = requests.request(
-                "GET", f"https://api.bitkub.com/api/market/ticker?sym=THB_{symbol}")
-            thb = float(response.json()[f"THB_{symbol}"]["last"])
-            cursor.execute(
-                f"select * from tbt_assets where symbol='{symbol}' and pair='THB'")
-
-            sql = f"update public.tbt_assets set symbol='{symbol}', pair='THB', price={thb}, lastupdate=CURRENT_TIMESTAMP where symbol='{symbol}' and pair='{currency}'"
-            if cursor.fetchone() == None:
-                sql = f"insert into public.tbt_assets(symbol,pair,price,lastupdate)values('{symbol}', 'THB',{thb},CURRENT_TIMESTAMP)"
-
-            cursor.execute(sql)
+        # For bitkub
+        response = requests.request(
+            "GET", f"https://api.bitkub.com/api/market/ticker?sym=THB_{symbol}")
+        thb = float(response.json()[f"THB_{symbol}"]["last"])
+        cursor.execute(
+            f"select * from tbt_assets where symbol='{symbol}' and pair='THB'")
+        sql = f"update public.tbt_assets set symbol='{symbol}', pair='THB', price={thb}, lastupdate=CURRENT_TIMESTAMP where symbol='{symbol}' and pair='{currency}'"
+        if cursor.fetchone() == None:
+            sql = f"insert into public.tbt_assets(symbol,pair,price,lastupdate)values('{symbol}', 'THB',{thb},CURRENT_TIMESTAMP)"
+        cursor.execute(sql)
+        print(f"{symbol}/THB ==> {response.status_code} price: {thb} THB: {thb}")
 
         print(f"---------------------------")
 
@@ -68,5 +67,25 @@ def main():
     conn.close()
 
 
+def sort_tbt_assets():
+    conn = psycopg2.connect(host=host, database=database,
+                            user=user, password=password)
+    # Create a cursor object to interact with the database
+    cursor = conn.cursor()
+    for x in crytoList:
+        cursor.execute(
+            f"select * from tbt_assets where symbol='{x}' order by symbol,price")
+        obj = cursor.fetchall()
+        if len(obj) > 1:
+            buyAt = obj[0]
+            sellAt = obj[len(obj) - 1]
+            print(
+                f"{buyAt[0]} buy at {buyAt[1]}::{buyAt[2]} and sell at {sellAt[1]}::{sellAt[2]} profit:{sellAt[2]-buyAt[2]}")
+
+    cursor.close()
+    conn.close()
+
+
 if __name__ == "__main__":
     main()
+    sort_tbt_assets()
