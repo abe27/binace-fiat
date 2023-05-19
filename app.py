8c93c7a10dcf3d28f72f8f2dc840493d67985c06
Taweechai Yuenyang <krumii.it@gmail.com>
2023-05-19 15:58:23 +0700

    add data to db

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import requests
+import psycopg2
 
 fiatList = ["AUD", "BRL", "EUR", "GBP", "RUB",
-            "TRY", "UAH", "ZAR", "IDRT", "NGN", "PLN", "RON", "ARS"]
+            "TRY", "UAH", "ZAR", "IDRT", "PLN", "RON", "ARS"]
 
 crytoList = ['BTC', 'ETH', 'ADA', 'USDT', 'XRP', 'LINK', 'MANA', 'XLM', 'BNB', 'DOGE', 'TRX', 'ALGO', 'BUSD', 'ENJ', 'DOT', 'NEAR', 'UNI',
              'SAND', 'GRT', 'MATIC', 'AXS', 'IMX', 'GALA', 'CHZ', 'SOL', 'AVAX', 'FTM', 'APE', 'GAL', 'OP', 'ATOM', 'ARB', 'ID']
 advCash = ["RUB", "EUR"]
+
+# Replace the placeholders with your actual database connection details
+host = 'localhost'
+database = 'arbitragedb'
+user = 'postgres'
+password = 'admin@1234'
 
 
 def lastPricePerTHB(currency):
@@ -17,6 +24,10 @@ def lastPricePerTHB(currency):
 
 
 def main():
+    conn = psycopg2.connect(host=host, database=database,
+                            user=user, password=password)
+    # Create a cursor object to interact with the database
+    cursor = conn.cursor()
     for symbol in crytoList:
         print(f"Start {symbol}:==>")
         for currency in fiatList:
@@ -28,8 +39,19 @@ def main():
                 thbPrice = lastPricePerTHB(currency)
                 print(
                     f"{symbol}/{currency} ==> {res.status_code} price: {obj['price']} THB: {float(obj['price']) * thbPrice}")
+                cursor.execute(
+                    f"select * from tbt_assets where symbol='{symbol}' and pair='{currency}'")
 
+                sql = f"update public.tbt_assets set symbol='{symbol}', pair='{currency}', price={float(obj['price']) * thbPrice}, lastupdate=CURRENT_TIMESTAMP where symbol='{symbol}' and pair='{currency}'"
+                if cursor.fetchone() == None:
+                    sql = f"insert into public.tbt_assets(symbol,pair,price,lastupdate)values('{symbol}', '{currency}',{float(obj['price']) * thbPrice},CURRENT_TIMESTAMP)"
+
+                cursor.execute(sql)
         print(f"---------------------------")
+
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
 if __name__ == "__main__":
